bdc5862809390d885b5211b5752e97e84fd35f79
Bernesh-stack <bernesh.in@gmail.com>
2025-07-04 22:10:27 +0530

    corrected for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
 import Home from './Pages/Home'
 import { Toaster } from 'sonner'
-import Quiz from "./Pages/Quiz";
+import Quiz from "./Pages/Quiz"
 import Feedback from './Pages/Feedback'
-function App() {
- 
 
+function App() {
   return (
-    <BrowserRouter>
-    <Toaster richColors position="top-right" />
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/quiz' element={<Quiz/>}/>
-      <Route path="/feedback" element={<Feedback />} />
-    </Routes>
-    </BrowserRouter>
+    <>
+      <Toaster richColors position="top-right" />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path="/feedback" element={<Feedback />} />
+      </Routes>
+    </>
   )
 }
 
